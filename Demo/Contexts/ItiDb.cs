32a1783ab55dc3b3a46eea9d49bd9c46b77411c0
khaled elbhdaly <155319819+kelbhdaly@users.noreply.github.com>
2025-04-03 17:32:09 +0200

    Add project files.

---
+++ Demo/Contexts/ItiDb.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+using Microsoft.EntityFrameworkCore;
+
+namespace Demo.Contexts
+{
+    internal class ItiDb :DbContext
+    {
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+    }
+}
